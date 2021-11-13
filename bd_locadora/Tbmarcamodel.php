e4d5054c08db9250ebc14f730b92ae35c2f938c6
Rosenilde <88213580+Rosenilde@users.noreply.github.com>
2021-11-13 11:24:20 -0300

    Add files via upload

---
+++ bd_locadora/Tbmarcamodel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Tbautomovelmodel extends Model
+{
+    protected $table      = 'tb_marca';
+    protected $primaryKey = 'TB_MARCA_ID';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = false;
+
+
+    protected $allowedFields =
+    ['	TB_MARCA_ID 
+      	TB_MARCA_NOME	'];
+    
+
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+
+    
+
+      
+      
+
+
+  }
